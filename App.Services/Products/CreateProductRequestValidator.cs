7d795c4953e719a2fe3265d529b514f168ab81fd
serkanyurttapan <serkan.yurttapan@farmasi.com.tr>
2025-01-05 19:24:16 +0300

    commit

---
+++ App.Services/Products/CreateProductRequestValidator.cs
@@ -0,0 +1,13 @@
+using FluentValidation;
+
+namespace Services.Products;
+
+public class CreateProductRequestValidator : AbstractValidator<CreateProductRequest>
+{
+    public CreateProductRequestValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+        RuleFor(x => x.Stock).GreaterThan(0).WithMessage("Stock must be greater than 0");
+    }
+}
